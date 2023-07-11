7a6662314e728499dd025aebdd2c1ecead6cc3bd
paradorn.rak <paradorn.rak@gmail.com>
2023-07-11 09:16:44 +0700

    [Fix] fix silly

--- ios/Classes/SwiftFlutterSocialPlatformSharePlugin.swift
+++ ios/Classes/SwiftFlutterSocialPlatformSharePlugin.swift
@@ -106,10 +106,12 @@ public class SwiftFlutterSocialPlatformSharePlugin: NSObject, FlutterPlugin, Sha
                                 facebookShareLink(quote, url: url)
                             }
                         } else {
-                            let fbSafariLink = "https://m.facebook.com/sharer/sharer.php?app_id=" + _fbAppId + "&u=" + url as String
-                            if let url = URL(string: fbSafariLink) {
-                                    UIApplication.shared.openURL(fbSafariLink)
+                            let fbSafariLink = "https://m.facebook.com/sharer/sharer.php?app_id=" + _fbAppId + "&u=" + url as? String
+                            if let fbUrl = URL(string: fbSafariLink) {
+                                if UIApplication.shared.canOpenURL(fbUrl) {
+                                    UIApplication.shared.openURL(fbUrl)
                                 }
+                            }
                             result(false)
                         }
                     }
